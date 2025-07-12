c731c217a47821b1aa57dc114cd560cd180730dd
Paco Chan <pacochan@Pacos-MacBook-Air.local>
2025-07-11 21:50:19 -0700

    fix 2

--- src/pages/prescribe/details/allergic-conjunctivitis.js
+++ src/pages/prescribe/details/allergic-conjunctivitis.js
@@ -9,12 +9,10 @@ const Details = () => {
   const handleClick = () => {
     setSite(prevSite => !prevSite);
   };
-  const handleCopy = async (text) => {
-    try {
-      await navigator.clipboard.writeText(text.toString());
-    } catch (err) {
-      console.error('Failed to copy: ', err);
-    }
+  const handleCopy = (text) => {
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 500);
   };
   return (
     <div>
@@ -57,9 +55,11 @@ const Details = () => {
             </tr>
             <tr className='formtitle'>
               <td colSpan={7}>
-                <button
-                  onClick={() => handleCopy(98890009)}
-                >Allergic Conjunctivitis</button>
+              <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800" onClick={() => handleCopy(98890009)}>
+                              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                              {copied ? 'Copied!' : 'Copy'}
+                              </span>                               
+                              </button>
               </td>
             </tr>
             <tr className='flex items-center justify-between '>
